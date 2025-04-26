0ec960c304adca198185e0319e346df75c68cc17
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2025-04-26 11:16:52 -0400

    main game class creates each room and adds them to the room array and sets the current room to be the worldmap1 room

--- src/Game.java
+++ src/Game.java
@@ -2,94 +2,29 @@ import java.awt.Graphics;
 
 public class Game extends GameBase {
 	
-	Player p = new Player(500, 500, 64);
-	
-//	static String[] filename = {
-//			"WorldMap1_Floor&Walls.txt", 
-//			"WorldMap1_Objects.txt"};
-	
-//	static String[] filename = {
-//			"WorldMap2_Floor&Walls.txt", 
-//			"WorldMap2_Objects.txt"};
-	
-//	static String[] filename = {
-//			"WorldMap3_Floor&Walls.txt", 
-//			"WorldMap3_Objects.txt",
-//			"WorldMap3_Border-Top-Left-Curve.txt",
-//			"WorldMap3_Border-Top-Right-Curve.txt",
-//			"WorldMap3_Border-Top.txt",
-//			"WorldMap3_Border-Bottom-Left-Curve.txt",
-//			"WorldMap3_Border-Bottom-Right-Curve.txt",
-//			"WorldMap3_Border-Bottom.txt",
-//			"WorldMap3_Border-Left.txt",
-//			"WorldMap3_Border-Right.txt"};
-	
-//	static String[] filename = {
-//			"Town_Floor&Walls.txt", 
-//			"Town_Objects.txt",
-//			"Town_Border-Bottom.txt",
-//			"Town_Border-Bottom-Right-Curve.txt",
-//			"Town_Border-Right.txt",
-//			"Town_Border-Bottom-Right.txt",
-//			"Town_Border-Top.txt",
-//			"Town_Border-Top-Left-Curve.txt",
-//			"Town_Border-Left.txt",
-//			"Town_Border-Top-Left.txt"};
-	
-//	static String[] filename = {
-//			"Dungeon1_Floor&Walls.txt", 
-//			"Dungeon1_Objects.txt"};
-	
-//	static String[] filename = {
-//			"Dungeon2_Floor&Walls.txt", 
-//			"Dungeon2_Objects.txt"};
-	
-//	static String[] filename = {
-//			"Dungeon3_Floor&Walls.txt", 
-//			"Dungeon3_Objects.txt"};
-	
-//	static String[] filename = {
-//			"Dungeon4_Floor&Walls.txt", 
-//			"Dungeon4_Objects.txt"};
-	
-//	static String[] filename = {
-//			"Dungeon5_Floor&Walls.txt", 
-//			"Dungeon5_Objects.txt"};
-//	
-	static String[] filename = {
-			"Dungeon6_Floor&Walls.txt", 
-			"Dungeon6_Objects.txt"};
-	
-	Map map = new Map(filename, 64);
-	
 	public void inGameLoop() {
-		if(pressing[UP])	p.moveUp(4);
-		if(pressing[DN])	p.moveDown(4);
-		if(pressing[LT])	p.moveLeft(4);
-		if(pressing[RT])	p.moveRight(4);
-		
-		checkWalls(p);
+		Room.current.inGameLoop();
 	}
 	
 	public void paint(Graphics pen) {
-		map.draw(pen);
-		p.draw(pen);
+		Room.current.draw(pen);
 	}
 
 	public void initialize() {
-
-	}
-	
-	public void checkWalls(Sprite s) {
-		for(int i = 0; i < map.wall.length; i++) {
-			if(s.overlaps(map.wall[i])) {
-				pushAway(s, map.wall[i]);
-			}
-		}
-	}
-	
-	public void pushAway(Sprite s, Rect r) {
-		s.pushAwayFrom(r);
+		Room.setUpInput(pressing);
+		
+		new WorldMap1();
+		new WorldMap2();
+		new WorldMap3();
+		new Town();
+		new Dungeon1();
+		new Dungeon2();
+		new Dungeon3();
+		new Dungeon4();
+		new Dungeon5();
+		new Dungeon6();
+		
+		Room.current = Room.room[0];
 	}
 
 }
