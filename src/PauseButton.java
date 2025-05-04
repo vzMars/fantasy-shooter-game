4efc18c07a483a719e416f4075289f927d0a0e27
HenryBatista1 <hbatista2003@gmail.com>
2025-05-03 22:07:00 -0400

    Created a PauseButton class
    It can pause or resume the game and the timer when the pause button is clicked

---
+++ src/PauseButton.java
@@ -0,0 +1,59 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.Timer;
+
+public class PauseButton extends Button{
+	
+	private Image pause;
+	private Image resume;
+	private Image img;
+	
+	public PauseButton() {
+		this(0, 0, 50);
+	}
+	public PauseButton(int x, int y, int scale) {
+		super("short", "", x, y, scale, scale);
+		
+		pause  = Toolkit.getDefaultToolkit().getImage("pause.png");
+		resume = Toolkit.getDefaultToolkit().getImage("resume.png");
+		
+		img       = pause;
+		isVisible = true;
+	}
+	
+	public void inGameLoop() {
+		
+		if(Game.mousePressed && !isPressed() && isPressed(Game.mx, Game.my));
+		
+		else if(!Game.mousePressed && isPressed()) {
+			release();
+			
+			if(Game.isPaused) resume();
+			else              pause();
+		}
+	}
+	
+	public void pause() {
+		if(Game.isPaused) return;
+		img = resume;
+		Game.isPaused = true;
+		Room.timer.pause();
+	}
+	
+	public void resume() {
+		if(Room.timer.hasEnded()) return;
+		img = pause;
+		Game.isPaused = false;
+		if(Room.timer.isStarted()) Room.timer.resume();
+		else                       Room.timer.start();
+	}
+	
+	
+	
+	public void draw(Graphics pen) {
+		super.draw(pen);
+		if(isVisible) pen.drawImage(img, x, y - 5, w, h, null);
+	}
+	
+}
