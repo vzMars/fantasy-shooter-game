0f2b2a852dc6d9628b6cf664b7ecd4b737a758af
bhaymalsingh21 <singhbhaymal806@gmail.com>
2025-05-18 01:59:39 -0400

    Cyclops and Golem class

--- src/Cyclops.java
+++ src/Cyclops.java
@@ -6,9 +6,9 @@ public class Cyclops extends EnemySprite {
 	public Cyclops(int x, int y, int scale) {
 		super("cyclops", x, y, scale, scale, direction, 3, 10, 100, 2);
 		
-		health  = 120;
-		setDamageGiven(12);
-		setForcePushback(20);
+		health  = 120; // Health of the enemy
+		setDamageGiven(12); // the damage it gives to the player
+		setForcePushback(20); // the push back value it gives to the player
 	
 	
 	}
--- src/Golem.java
+++ src/Golem.java
@@ -3,32 +3,14 @@ public class Golem extends EnemySprite {
 	static String[] direction = { "up", "dn", "lt", "rt"};
 
 	public Golem(int x, int y, int scale) {
+		//(String name, int x, int y, int w, int h, String[] pose, int count, int duration, int health, int chaseSpeed)
 		super("golem", x, y, scale, scale, direction, 3, 10, 100,1);
 	
 		
 		setDamageGiven(30);
 		setForcePushback(40);
-		
-		
-		
-		
-		
-		
-		
-	}
-
 	
-
-	@Override
-	public void setDamageGiven(int amt) {
-		
-		this.damageGive = amt;
 		
 	}
 
-	@Override
-	public void setForcePushback(int amt) {
-		this.forcePushBack = amt;
-	}
-	
 }
